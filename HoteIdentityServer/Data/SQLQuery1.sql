ed996d5238e272601a5f26cf3b5363b9bba16f1e
THOMAS FERNEZ <TFE15965@open-groupe.com>
2024-04-09 14:41:35 +0200

    Ajout IdentityServer

---
+++ HoteIdentityServer/Data/SQLQuery1.sql
@@ -0,0 +1,12 @@
+﻿--Aparent@joboverview.fr / @Pass1 
+--Bnormand@joboverview.fr / @Pass2 
+--Jrousse@joboverview.fr / @Pass3 
+
+update AspNetUsers set id = 'APARENT', UserName = 'Agnès Parent'
+where UserName = 'aparent@joboverview.fr'
+update AspNetUsers set id = 'BNORMAND', UserName = 'Bertrand Normand'
+where UserName = 'bnormand@joboverview.fr'
+update AspNetUsers set id = 'JROUSSET', UserName = 'Joseph Rousset'
+where UserName = 'jrousset@joboverview.fr'
+
+select * from AspNetUsers
